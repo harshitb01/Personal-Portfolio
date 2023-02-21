4c3b3cde5561e5c429af61b06079a68195d75248
Harshit <harshitb2006@gmail.com>
2023-02-21 16:22:37 +0530

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,48 +16,43 @@ const Services = () => {
                     <ul className="service__list">
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Worked as the Head of Graphic Team (Responsible for Game Designing)</p>
+                            <p>Led graphic design team for mobile game promoting organ donation.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Helped in spreading awareness about organ donation by creating a Mobile Game</p>
+                            <p>Designed game assets using Photoshop and implemented in Unity Engine.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Created Assets of the game to accomplish the goal by using Unity Engine</p>
+                            <p>Placed game on NGO website portal, planned for Google Play Store.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>
-                                Successful in getting the game placed on the NGO website portal and on play store in
-                                future
-                            </p>
+                            <p>Developed engaging game to effectively promote NGO's cause.</p>
                         </li>
                     </ul>
                 </article>
                 <article className="service">
                     <div className="service__head">
-                        <h3>Prope Network & Cafe Code Brew</h3>
-                        <h3>(Freelancing)</h3>
+                        <h3>Freelancing</h3>
+                        <h3>(Graphic Designer)</h3>
                     </div>
                     <ul className="service__list">
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Worked as a freelance Graphic Designer</p>
+                            <p>Designed logos for Prope Network, Nearbii App, and Cafe Code Brew.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Designed Few logos for The Prope Network and The Nearbii App</p>
+                            <p>Iterated designs based on feedback to ensure client satisfaction.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>
-                                Provide them with editable files so that they may make modifications in the future.
-                            </p>
+                            <p>Created scalable logos using Adobe Photoshop for marketing collateral.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Made Logos with the help of Adobe Photoshop</p>
+                            <p>Provided clients with editable logo files for future modifications.</p>
                         </li>
                     </ul>
                 </article>
@@ -69,29 +64,19 @@ const Services = () => {
                     <ul className="service__list">
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Developed My Portfolio Website Using React</p>
+                            <p>Developed arcade games using Unity with engaging gameplay and visuals.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Successfully Uploaded it to cloud</p>
+                            <p>Collaborated with designers and developers for user interface and experience.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>Created separate Components for each Section of the website</p>
+                            <p>Maintained organized code repositories with Git version control.</p>
                         </li>
                         <li>
                             <BiCheck className="service__list-icon" />
-                            <p>
-                                Made Sure that the code was arranged such that anybody could understand it with
-                                ease.
-                            </p>
-                        </li>
-                        <li>
-                            <BiCheck className="service__list-icon" />
-                            <p>
-                                It was Designed responsive in order to be easily accessible on Mobile, tablets and
-                                laptops.
-                            </p>
+                            <p>Implemented clean, well-documented, and maintainable code for high-quality games.</p>
                         </li>
                     </ul>
                 </article>
