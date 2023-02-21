871de522e753da734c53c097f2c6db59be2d89f4
Harshit <harshitb2006@gmail.com>
2023-02-21 17:24:59 +0530

    nav bar fixed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,8 +3,9 @@ import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
 import { RiServiceLine } from "react-icons/ri";
-import { BiMessageSquareDetail } from "react-icons/bi";
+import { RiLinksLine } from "react-icons/ri";
 
 const Nav = () => {
     const [activeNav, setActiveNav] = useState("#");
@@ -57,11 +58,18 @@ const Nav = () => {
                 <RiServiceLine />
             </a>
             <a
+                href="#portfolio"
+                onClick={() => setActiveNav("#portfolio")}
+                className={activeNav === "#portfolio" ? "active" : ""}
+            >
+                <RiLinksLine />
+            </a>
+            <a
                 href="#contact"
                 onClick={() => setActiveNav("#contact")}
                 className={activeNav === "#contact" ? "active" : ""}
             >
-                <BiMessageSquareDetail />
+                <MdOutlinePermContactCalendar />
             </a>
         </nav>
     );
